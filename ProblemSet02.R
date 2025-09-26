f1b755f73d9351ed3b43a8a98555e8aad25d8d3c
Colin Purtell <cpurtell13@gmail.com>
2025-09-26 11:06:22 -0400

    Fixed random walk 2

--- ProblemSet02.R
+++ ProblemSet02.R
@@ -26,12 +26,12 @@ random_walk1 <- function(n, seed = NULL) {
 
 random_walk2 <- function(n, seed = NULL) {
   if (!is.null(seed)) set.seed(seed)
-  steps <- sample(
-    x = c(+1, +10, -1, -3), #Number of steps
-    size = n,
-    replace = TRUE,
-    prob = c(0.5*0.95, 0.5*0.05, 0.5*0.8, 0.5*0.2) #Probs for above step lengths
-  )
+  u <- runif(2 * n)
+  dir <- u[seq(1, 2*n, by = 2)] < 0.5    #Direction
+  sub <- u[seq(2, 2*n, by = 2)]          #Subtype
+  steps <- ifelse(dir,
+                  ifelse(sub < 0.05, 10, 1),   
+                  ifelse(sub < 0.2, -3, -1))
   
   sum(steps)
 }
@@ -64,3 +64,23 @@ random_walk3(10, seed=42)
 random_walk1(1000, seed=42)
 random_walk2(1000, seed=42)
 random_walk3(1000, seed=42)
+
+
+### c. ###
+library(microbenchmark)
+
+#For low input
+microbenchmark(
+  loop = random_walk1(1000),
+  vectorized = random_walk2(1000),
+  apply = random_walk3(1000),
+  times = 100
+)
+
+#For high input
+microbenchmark(
+  loop = random_walk1(100000),
+  vectorized = random_walk2(100000),
+  apply = random_walk3(100000),
+  times = 10
+)
